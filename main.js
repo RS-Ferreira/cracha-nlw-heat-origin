bd749de3a065de0e0b014f400446c1402ae6b185
Ronaldo Ferreira <86159115+RS-Ferreira@users.noreply.github.com>
2021-10-23 14:41:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'RS-Ferreira',
+  youtube: '',
+  facebook: 'ronaldo.ferreira.92754397',
+  instagram: 'ronaldo.s.ferreira',
+  twitter: ''
+}
+
+function changeSocialMedialLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMedialLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
